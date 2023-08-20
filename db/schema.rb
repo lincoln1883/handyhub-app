9574e2233525e265619391fd3637e520d36de45f
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-19 23:45:02 -0300

    Create job model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_20_023851) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_20_024315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.text "description"
+    t.boolean "is_taken"
+    t.boolean "is_completed"
+    t.decimal "high_price", precision: 10, scale: 2
+    t.decimal "low_price", precision: 10, scale: 2
+    t.date "expiration_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
