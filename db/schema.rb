ac63a958708a049a8c0e12d2941365a48abae415
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-19 23:41:45 -0300

    Create city model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_20_023409) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_20_023851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trades", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
