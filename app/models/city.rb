c8064c09972e70af01dba4cd069c9a4e3a1c9053
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-09 20:25:49 -0300

    Add validation and relations.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,6 @@
 class City < ApplicationRecord
-  validates :name, presence: true, nullable: false
+  belongs_to :country
+  has_many :jobs
+  has_many :supplier_cities, class_name: 'User', foreign_key: :is_supplier
+  has_many :suppliers, through: :jobs
 end
